3ec79dd9041eb1cfce42026495cafe43bfd49cfa
sanctum32 <andriuspel@gmail.com>
2020-03-29 04:24:15 +0300

    Core/Build: implemented new cmake option to disable/enable errors armagedon within MSVC builds

--- cmake/Compilers/msvc.cmake
+++ cmake/Compilers/msvc.cmake
@@ -17,11 +17,18 @@ if(NOT IS_64BIT)
     set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /LARGEADDRESSAWARE")
 endif()
 
+# Treat warnings as errors
+# Note: compiler warnings aren't always bug free...
+if (BUILD_ERRORS_MODE)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /WX")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /WX")
+endif()
+
 # enable/disable warnings
 # dll warning 4251 disabled by default.
 if (BUILD_WITH_WARNINGS)
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /WX /wd4251")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /WX /wd4251")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /wd4251")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4251")
 else()
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /W0")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W0")
